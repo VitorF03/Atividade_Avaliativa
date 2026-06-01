a1750548138b42cd9a9ee6b20da4a4dee6a85a82
VitorF03 <vitorm06070312@gmail.com>
2026-06-01 16:45:57 -0300

    Add files via upload

---
+++ idade.cs
@@ -0,0 +1,14 @@
+﻿int ano = 0;
+int atual = 2026;
+
+Console.WriteLine("Verificador de Idade");
+
+Console.WriteLine("Informe o ano em que você nasceu: ");
+ano = Convert.ToInt16(Console.ReadLine());
+
+int idade = (atual - ano);
+
+if (idade >= 18)
+    Console.WriteLine($"Você nasceu em {ano}, sua idade é {idade} e você já é maior de idade, ou seja, pode tirar CNH.");
+else 
+    Console.WriteLine($"Você nasceu em {ano}, sua idade é {idade} e você é menor de idade, ou seja, ainda não pode tirar sua CNH.");
